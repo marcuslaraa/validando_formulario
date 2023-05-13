37ba5ddafb563602c9523cd7945ac34b79f46883
Marcus Vinícius Lara <marcus.lara@hotmail.com>
2023-05-13 17:00:50 -0300

    Validando cpf

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import ehUmCpf from "./valida-cpf.js";
+
+const camposDoFormulario = document.querySelectorAll("[required]");
+
+function verificaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCpf(campo)
+}}
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo))
+})
